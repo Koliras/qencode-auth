0be83937bc31d8063084640e94fc4dd7ef11a236
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2024-03-18 13:42:13 +0200

    add post request to login form

--- src/components/forms/LogInForm.jsx
+++ src/components/forms/LogInForm.jsx
@@ -2,8 +2,28 @@ import './form.css';
 import GoogleLogo from '../../assets/GoogleLogo.svg'
 import GithubLogo from '../../assets/GithubLogo.svg'
 import { Link } from 'react-router-dom'
+import { AuthAxios } from '../../utils/AuthAxios';
+import { useState } from 'react';
 
 export default function LogInForm() {
+  const [pass, setPass] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await AuthAxios.post("/login", {
+      email: email, 
+      password: pass,
+    });
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePassChange = (event) => {
+    setPass(event.target.value);
+  }
   return (
     <div className='formContainer'>
       <h2 className="formHeading">Log in to your account</h2>
@@ -21,12 +41,14 @@ export default function LogInForm() {
 
       <span className='devider mb30'>Or</span>
 
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <input
           type="email"
           required
           className='input mb24'
           placeholder='Work email'
+          value={email}
+          onChange={handleEmailChange}
         />
 
         <input
@@ -35,6 +57,8 @@ export default function LogInForm() {
           className='input mb16'
           minLength={8}
           placeholder='Password'
+          value={pass}
+          onChange={handlePassChange}
         />
 
         <Link to='/forgot-pass' className='forgotPass mb30'>
---
+++ src/utils/AuthAxios.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export const AuthAxios = axios.create({
+  baseURL: "https://auth-qa.qencode.com/v1/auth",
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+})
